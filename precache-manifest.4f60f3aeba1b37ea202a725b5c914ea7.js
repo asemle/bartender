696955848c3705fbd95b03ceaeab564e29c881b4
asemle <asemle@umass.edu>
2019-10-21 15:05:17 -0400

    Updates

---
+++ precache-manifest.4f60f3aeba1b37ea202a725b5c914ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e94734e1614fc732608592629bcb4e",
+    "url": "/bartender/index.html"
+  },
+  {
+    "revision": "a13dceba915075889034",
+    "url": "/bartender/static/css/main.e0f0295b.chunk.css"
+  },
+  {
+    "revision": "abc0c0aa92a38fdc81d6",
+    "url": "/bartender/static/js/2.b9998436.chunk.js"
+  },
+  {
+    "revision": "a13dceba915075889034",
+    "url": "/bartender/static/js/main.2f8680b9.chunk.js"
+  },
+  {
+    "revision": "7663913515fede292ad2",
+    "url": "/bartender/static/js/runtime-main.c3aa3ad0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bartender/static/media/logo.25bf045c.svg"
+  }
+]);
